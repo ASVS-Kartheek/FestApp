a7a1fd994a4fde9d302e84ecaaea340f950d96b3
Kartheek Akella <sukruthkartheek@gmail.com>
2018-11-09 07:33:32 +0530

    Add Dagger dependencies

--- app/src/main/java/com/example/kartheek/festapp/MainActivity.kt
+++ app/src/main/java/com/example/kartheek/festapp/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.kartheek.festapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
