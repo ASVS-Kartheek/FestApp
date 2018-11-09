882aa32906a9c8bc7e9fa96e02003a5c1dee00f4
Kartheek Akella <sukruthkartheek@gmail.com>
2018-11-09 08:16:18 +0530

    Create new package for ui

--- app/src/main/java/com/example/kartheek/festapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/kartheek/festapp/ui/MainActivity.kt
@@ -1,11 +1,17 @@
-package com.example.kartheek.festapp
+package com.example.kartheek.festapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kartheek.festapp.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val FRAGMENT_ID = R.id.fragment_container
+    }
+
+    private lateinit var currentFragment: MainNavigationFragment
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -34,5 +40,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        // Add a listener to prevent reselects from being treated as selects.
+        navigation.setOnNavigationItemReselectedListener {}
+
+        if (savedInstanceState == null) {
+            // Show Schedule on first creation
+            navigation.selectedItemId = R.id.navigation_schedule
+        } else {
+            // Find the current fragment
+            currentFragment =
+                    supportFragmentManager.findFragmentById(FRAGMENT_ID) as? MainNavigationFragment
+                    ?: throw IllegalStateException("Activity recreated, but no fragment found!")
+        }
     }
 }
